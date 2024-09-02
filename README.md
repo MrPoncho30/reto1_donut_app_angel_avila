3d83baec3b1fce81a640919023c99cc5399f2991
MrPoncho30 <alfonsoavilag30@gmail.com>
2024-09-02 14:03:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_angel_avila
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_angel_avila/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_angel_avila
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
