a6c665e476823e9cd69f051a1402edc5d84d3fcd
MrPoncho30 <alfonsoavilag30@gmail.com>
2024-09-09 15:42:03 -0600

    Tabs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:reto1_donut_app_angel_avila/tab/burger_tab.dart';
+import 'package:reto1_donut_app_angel_avila/tab/donut_tab.dart';
+import 'package:reto1_donut_app_angel_avila/tab/pancakes_tab.dart';
+import 'package:reto1_donut_app_angel_avila/tab/pizza_tab.dart';
+import 'package:reto1_donut_app_angel_avila/tab/smoothie_tab.dart';
+import 'package:reto1_donut_app_angel_avila/utils/my_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,51 +14,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+
+    //burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+
+    //smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+
+    //pancakes tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 24.0),
-          child: IconButton(
-            icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
-            onPressed: () {
-              print('Botón de Menú');
-            },
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
             child: IconButton(
-              icon: Icon(Icons.person, color: Colors.grey[800], size: 36),
+              icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
               onPressed: () {
-                print('Botón de Usuario');
+                print('Botón de Menú');
               },
             ),
-          )
-        ],
-      ),
-      body: const Column(children: [
-        //TEXTO "I want to eat"
-        Padding(
-          padding: EdgeInsets.all(24.0),
-          child: Row(
-            children: [
-              Text("I want to ", style: TextStyle(fontSize: 32)),
-              Text("Sleep",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline)),
-            ],
           ),
-        )
-        //Tap Bar
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: IconButton(
+                icon: Icon(Icons.person, color: Colors.grey[800], size: 36),
+                onPressed: () {
+                  print('Botón de Usuario');
+                },
+              ),
+            )
+          ],
+        ),
+        body: Column(children: [
+          //TEXTO "I want to eat"
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text("I want to ", style: TextStyle(fontSize: 32)),
+                Text("Eat",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline)),
+              ],
+            ),
+          ),
+          //Tap Bar
+          TabBar(tabs: myTabs),
+          //Tap bar view
+          const Expanded(
+            child: TabBarView(children: [
+              //Donut tab
+              DonutTab(),
 
-        //Tap view
-      ]),
+              //Burger tab
+              BurgerTab(),
+
+              //Smoothie tab
+              SmoothieTab(),
+
+              //Pancakes tab
+              PancakesTab(),
+
+              //Pizza tab
+              PizzaTab(),
+            ]),
+          )
+          //Carrito
+        ]),
+      ),
     );
   }
 }
